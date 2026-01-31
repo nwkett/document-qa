53f12f81c6c518ef6b2dc9b13d193abd42fcff0c
nwkett <113996906+nwkett@users.noreply.github.com>
2026-01-31 00:08:39 +0000

    fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,8 +5,8 @@ st.set_page_config(page_title="Labs", layout="wide")
 # Create a navigation object with pages
 pg = st.navigation(
     [
-        st.Page("pages/Lab1.py", title="Lab 1", icon="1️⃣"),
-        st.Page("pages/Lab2.py", title="Lab 2", icon="2️⃣", default=True),
+        st.Page("pages/lab1.py", title="Lab 1", icon="1️⃣"),
+        st.Page("pages/lab2.py", title="Lab 2", icon="2️⃣", default=True),
     ]
 )
 
